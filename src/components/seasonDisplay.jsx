45eab6cd843dd1297e6f4fc092db78cc8205ef9f
Boitumelo_Iven Moshitwa <itumelo553@gmail.com>
2023-08-11 19:06:23 +0200

    add changes

--- src/components/seasonDisplay.jsx
+++ src/components/seasonDisplay.jsx
@@ -1,13 +1,30 @@
+import { set } from "mongoose";
 import React, { useState, useEffect } from "react";
 
 const SeasonDisplay = () => {
-  const [state, setState] = useState({ lat: null });
+  const [state, setState] = useState({ lat: null, errMessage: "" });
 
   useEffect(() => {
-    window.navigator.geolocation.getCurrentPosition((position) => {
-      setState({ lat: position.coords.latitude });
-    });
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        setState({ errMessage: err.message });
+      }
+    );
   }, []);
-  return <h1>Latitude:{state} </h1>;
+  return (
+    <h1>
+      Latitude:
+      {state.map(latitude, (i) => {
+        return (
+          <div key={i}>
+            <h1>Latitude: {latitude.latitude}</h1>
+          </div>
+        );
+      })}
+    </h1>
+  );
 };
 export default SeasonDisplay;
